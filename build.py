db25ed5acc43cf36b5790558820bd25259b893c4
deagahelio <16138291+deagahelio@users.noreply.github.com>
2018-12-14 22:49:59 -0200

    libc

--- build.py
+++ build.py
@@ -3,7 +3,14 @@ from pathlib import Path
 
 def run(command):
 	print(command)
-	return os.system(command)
+	code = os.system(command)
+	if code != 0:
+		print()
+		print()
+		print(f" !!! COMMAND EXITED WITH CODE {code}, ABORTING !!! ")
+		print()
+		print()
+		sys.exit(1)
 
 linker_script = "linker.ld"
 
@@ -18,10 +25,12 @@ link_flags = f"-T {linker_script} -ffreestanding -O2 -nostdlib -lgcc"
 
 obj_dir = "obj/"
 kernel_dir = "kernel/"
+libc_dir = "libc/"
 asm_dir = f"{kernel_dir}asm/"
 
 obj_files = glob.iglob(obj_dir + "**/*.o", recursive=True)
 kernel_files = glob.iglob(kernel_dir + "**/*.c", recursive=True)
+libc_files = glob.iglob(libc_dir + "**/*.c", recursive=True)
 asm_files = glob.iglob(asm_dir + "**/*.asm", recursive=True)
 
 iso_dir = "iso/"
@@ -34,9 +43,13 @@ def assemble():
 	for file in asm_files:
 		run(f"{assembler} {assembler_flags} {file} -o {obj_dir}{Path(file).stem + '.o'}")
 
+def compile_libc():
+	for file in libc_files:
+		run(f"{compiler} {compile_flags} -c {file} -I{libc_dir}include -o {obj_dir}{Path(file).stem + '.o'} {compiler_flags}")
+
 def compile_kernel():
 	for file in kernel_files:
-		run(f"{compiler} {compile_flags} -c {file} -o {obj_dir}{Path(file).stem + '.o'} {compiler_flags}")
+		run(f"{compiler} {compile_flags} -c {file} -I{libc_dir}include -I{kernel_dir}include -o {obj_dir}{Path(file).stem + '.o'} {compiler_flags}")
 
 def link():
 	run(f"{compiler} {link_flags} {' '.join(list(obj_files))} -o {bin_file} {compiler_flags}")
@@ -44,6 +57,13 @@ def link():
 def make_iso():
 	run(f"cp {bin_file} {iso_dir}boot/")
 	run(f"grub-mkrescue -o {iso_file} {iso_dir}")
+
+def all():
+	assemble()
+	compile_libc()
+	compile_kernel()
+	link()
+	make_iso()
 
 def clean():
 	run(f"rm {bin_file}")
@@ -53,45 +73,42 @@ def clean():
 def qemu():
 	run(f"qemu-system-i386 -cdrom {iso_file}")
 
-print()
-print(" *** TsukiOS Builder ***")
-print()
-
-
-if len(sys.argv) != 2:
-	print(f" Usage: {sys.argv[0]} <target>")
+if __name__ == "__main__":
 	print()
-	print(" Available Targets:")
-	print("   assemble, kernel, link, iso, all, clean, qemu")
+	print(" *** TsukiOS Builder ***")
 	print()
-	sys.exit(1)
 
-target = sys.argv[1].strip().lower()
 
-if target == "assemble":
-	assemble()
-elif target == "kernel":
-	compile_kernel()
-elif target == "link":
-	link()
-elif target == "iso":
-	make_iso()
-elif target == "all":
-	assemble()
-	compile_kernel()
-	link()
-	make_iso()
-elif target == "clean":
-	clean()
-elif target == "qemu":
-	assemble()
-	compile_kernel()
-	link()
-	make_iso()
-	qemu()
-else:
-	print("Unknown target, exiting")
+	if len(sys.argv) != 2:
+		print(f" Usage: {sys.argv[0]} <target>")
+		print()
+		print(" Available Targets:")
+		print("   assemble, kernel, link, iso, all, clean, qemu")
+		print()
+		sys.exit(1)
+
+	target = sys.argv[1].strip().lower()
+
+	if target == "assemble":
+		assemble()
+	elif target == "libc":
+		compile_libc()
+	elif target == "kernel":
+		compile_kernel()
+	elif target == "link":
+		link()
+	elif target == "iso":
+		make_iso()
+	elif target == "all":
+		all()
+	elif target == "clean":
+		clean()
+	elif target == "qemu":
+		all()
+		qemu()
+	else:
+		print("Unknown target, exiting")
+		print()
+		sys.exit(1)
+
 	print()
-	sys.exit(1)
-
-print()
---
+++ kernel/include/terminal.h
@@ -0,0 +1,36 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GRAY = 7,
+	VGA_COLOR_DARK_GRAY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+void terminal_init(void);
+void terminal_setcolor(uint8_t color);
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+void terminal_putchar(char c);
+void terminal_write(const char* data, size_t size);
+void terminal_writestring(const char* data);
+
+#endif
