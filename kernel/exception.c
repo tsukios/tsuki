96a2a9fbb0a1a782fbab841936d14cd0883a61f2
deagahelio <16138291+deagahelio@users.noreply.github.com>
2018-12-22 23:43:37 -0200

    Add more exception handlers

--- kernel/exception.c
+++ kernel/exception.c
@@ -7,8 +7,13 @@
 void exception_init(void)
 {
 	idt_encode_entry(idt[0], &exception_divide_by_zero, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);
+	idt_encode_entry(idt[4], &exception_overflow, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);	
+	idt_encode_entry(idt[5], &exception_bound_range_exceeded, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);	
+	idt_encode_entry(idt[6], &exception_invalid_opcode, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);	
+	idt_encode_entry(idt[7], &exception_device_not_available, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);	
 	idt_encode_entry(idt[8], &exception_double_fault, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);
 	idt_encode_entry(idt[14], &exception_page_fault, IDT_RING_ZERO | IDT_PRESENT | IDT_INTERRUPT_GATE);
+
 	log(LOG_OK, "Exception module initialized\n");
 }
 
@@ -16,6 +21,30 @@ __attribute__((interrupt))
 void exception_divide_by_zero(struct isr_interrupt_frame* frame)
 {
 	panic_exception(0, frame);
+}
+
+__attribute__((interrupt))
+void exception_overflow(struct isr_interrupt_frame* frame)
+{
+	panic_exception(4, frame);
+}
+
+__attribute__((interrupt))
+void exception_bound_range_exceeded(struct isr_interrupt_frame* frame)
+{
+	panic_exception(5, frame);
+}
+
+__attribute__((interrupt))
+void exception_invalid_opcode(struct isr_interrupt_frame* frame)
+{
+	panic_exception(6, frame);
+}
+
+__attribute__((interrupt))
+void exception_device_not_available(struct isr_interrupt_frame* frame)
+{
+	panic_exception(7, frame);
 }
 
 __attribute__((interrupt))
--- kernel/include/exception.h
+++ kernel/include/exception.h
@@ -5,6 +5,10 @@
 
 void exception_init(void);
 __attribute__((interrupt)) void exception_divide_by_zero(struct isr_interrupt_frame* frame);
+__attribute__((interrupt)) void exception_overflow(struct isr_interrupt_frame* frame);
+__attribute__((interrupt)) void exception_bound_range_exceeded(struct isr_interrupt_frame* frame);
+__attribute__((interrupt)) void exception_invalid_opcode(struct isr_interrupt_frame* frame);
+__attribute__((interrupt)) void exception_device_not_available(struct isr_interrupt_frame* frame);
 __attribute__((interrupt)) void exception_double_fault(struct isr_interrupt_frame* frame);
 __attribute__((interrupt)) void exception_page_fault(struct isr_interrupt_frame* frame);
 
